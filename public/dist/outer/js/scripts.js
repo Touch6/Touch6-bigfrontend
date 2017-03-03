93f1f93e651c5cd2a39c345939d0f26cb1bf5a08
v5zhu@qq.com <longzhu20*>
2017-03-03 10:39:36 +0800

    合并以前的demo文件

---
+++ public/dist/outer/js/scripts.js
@@ -0,0 +1,51 @@
+function initializeJS() {
+
+    //tool tips
+    jQuery('.tooltips').tooltip();
+
+    //popovers
+    jQuery('.popovers').popover();
+
+    //custom scrollbar
+        //for html
+    jQuery("html").niceScroll({styler:"fb",cursorcolor:"#007AFF", cursorwidth: '6', cursorborderradius: '10px', background: '#F7F7F7', cursorborder: '', zindex: '1000'});
+        //for sidebar
+    jQuery("#sidebar").niceScroll({styler:"fb",cursorcolor:"#007AFF", cursorwidth: '3', cursorborderradius: '10px', background: '#F7F7F7', cursorborder: ''});
+        // for scroll panel
+    jQuery(".scroll-panel").niceScroll({styler:"fb",cursorcolor:"#007AFF", cursorwidth: '3', cursorborderradius: '10px', background: '#F7F7F7', cursorborder: ''});
+
+
+    // sidebar menu toggle
+    jQuery(function() {
+        function responsiveView() {
+            var wSize = jQuery(window).width();
+            if (wSize <= 768) {
+                jQuery('#container').addClass('sidebar-close');
+                jQuery('#sidebar > ul').hide();
+            }
+
+            if (wSize > 768) {
+                jQuery('#container').removeClass('sidebar-close');
+                jQuery('#sidebar > ul').show();
+            }
+        }
+        jQuery(window).on('load', responsiveView);
+        jQuery(window).on('resize', responsiveView);
+    });
+
+    //bar chart
+    if (jQuery(".custom-custom-bar-chart")) {
+        jQuery(".bar").each(function () {
+            var i = jQuery(this).find(".value").html();
+            jQuery(this).find(".value").html("");
+            jQuery(this).find(".value").animate({
+                height: i
+            }, 2000)
+        })
+    }
+
+}
+
+jQuery(document).ready(function(){
+    initializeJS();
+});
