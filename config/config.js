79183002341776cdfd79a03be66a231210a96f62
v5zhu@qq.com <longzhu20*>
2017-03-03 10:13:32 +0800

    合并以前的demo文件

---
+++ config/config.js
@@ -0,0 +1,15 @@
+var config;
+config = {
+    wcs: {
+        "scope": "chop-app",
+        "deadline": '1451577600000',
+        "returnBody": 'fname=$(fname)&fsize=$(fsize)&url=$(url)',
+        "overwrite": 0,
+        "WSC_AK": "32587e8e55b27268ec49ba7f0a56fb03a58024ec",
+        "WSC_SK": "044a34a77be176273523267e33a57080d6e9a4b4"
+    }
+};
+
+module.exports = config;
+
+module.exports.config = config;
