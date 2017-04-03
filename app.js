477191a2b1db1f74294023901a92f97358a9f66c
LONG <v5zhu@qq.com>
2017-04-03 17:37:49 +0800

    NODE_ENV=development 运行配置环境变量

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 /********设置nodejs路由对应的文件*************/
-var home=require('./routes/user/home');
-var ccap=require('./routes/ccap');
-var login=require('./routes/user/login');
-var register=require('./routes/user/register');
-var phone=require('./routes/user/phone');
-var toutiao=require('./routes/toutiao/toutiao');
-var tools=require('./routes/tools/tools');
+var home = require('./routes/user/home');
+var ccap = require('./routes/ccap');
+var login = require('./routes/user/login');
+var register = require('./routes/user/register');
+var phone = require('./routes/user/phone');
+var toutiao = require('./routes/toutiao/toutiao');
+var tools = require('./routes/tools/tools');
 
-var sendEmail=require('./routes/email');
-var touchcApp=require('./routes/touchcApp/touchcApp');
-var uptoken=require('./routes/uptoken');
-var account=require('./routes/user/account');
+var sendEmail = require('./routes/email');
+var touchcApp = require('./routes/touchcApp/touchcApp');
+var uptoken = require('./routes/uptoken');
+var account = require('./routes/user/account');
 
 var app = express();
 // view engine setup
@@ -31,12 +31,12 @@ app.set('view engine', 'jade');
 var log4js = require('log4js');
 log4js.configure({
     appenders: [
-        { type: 'console' }, //控制台输出
+        {type: 'console'}, //控制台输出
         {
             type: 'file', //文件输出
             filename: 'logs/access.log',
             maxLogSize: 1024,
-            backups:3,
+            backups: 3,
             category: 'normal'
         }
     ],
@@ -44,39 +44,36 @@ log4js.configure({
 });
 var logger = log4js.getLogger('normal');
 logger.setLevel('debug');
-app.use(log4js.connectLogger(logger, {level:log4js.levels.DEBUG, format:':method :url'}));
+app.use(log4js.connectLogger(logger, {level: log4js.levels.DEBUG, format: ':method :url'}));
 
 // app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
 
-app.use('/home',home);
+app.use('/home', home);
 //验证码；
-app.use('/ccap',ccap);
-app.use('/login',login);
-app.use('/register',register);
-app.use('/phone',phone);
-app.use('/toutiao',toutiao);
-app.use('/tools',tools);
-app.use('/sendEmail',sendEmail);
-app.use('/touchcApp',touchcApp);
-app.use('/uptoken',uptoken);
-app.use('/account',account);
-
-
-
+app.use('/ccap', ccap);
+app.use('/login', login);
+app.use('/register', register);
+app.use('/phone', phone);
+app.use('/toutiao', toutiao);
+app.use('/tools', tools);
+app.use('/sendEmail', sendEmail);
+app.use('/touchcApp', touchcApp);
+app.use('/uptoken', uptoken);
+app.use('/account', account);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -84,22 +81,31 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    console.log("development");
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
+} else if (app.get('env') === 'test') {
+    console.log("test");
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+} else if (app.get('env') === 'production') {
+    console.log("production");
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
 }
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
 module.exports = app;
